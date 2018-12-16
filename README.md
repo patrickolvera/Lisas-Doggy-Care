d0b33e475c1d1799973d2b49301b4a08895bd514
Patrick Olvera <patrickolvera@gmail.com>
2018-12-16 14:32:09 -0600

    Docs: README, LICENSE, and minor changes

---
+++ README.md
@@ -0,0 +1,7 @@
+# Lisa's Doggy Care
+
+This is a website I built for a dog boarding business. I am sharing the source code with the owner's permission. You can view the site live at [`lisasDoggyCare.com`](https://www.lisasdoggycare.com).
+
+## License
+
+This project is licensed under a Creative Commons Attribution 3.0 United States License. View [`LICENSE`](LICENSE) for more information.
--- assets/js/main.js
+++ assets/js/main.js
@@ -476,16 +476,16 @@
 
 	// Service Worker Installation
 
-	if ('serviceWorker' in navigator) {
-		window.addEventListener('load', () => {
-			navigator.serviceWorker.register('/sw.js')
-				.then(registration => {
-					console.log('ServiceWorker registration successful with scope: ', registration.scope);
-				})
-				.catch(err => {
-					console.error('ServiceWorker registration failed: ', err);
-				});
-		});
-	}
+	// if ('serviceWorker' in navigator) {
+	// 	window.addEventListener('load', () => {
+	// 		navigator.serviceWorker.register('/sw.js')
+	// 			.then(registration => {
+	// 				console.log('ServiceWorker registration successful with scope: ', registration.scope);
+	// 			})
+	// 			.catch(err => {
+	// 				console.error('ServiceWorker registration failed: ', err);
+	// 			});
+	// 	});
+	// }
 
 })(jQuery);
